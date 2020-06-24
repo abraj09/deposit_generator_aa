08941f561cb52d25d6bc9b34bbf13628849d4b8d
ABHIJEET RAJ <abhijeet@backprop.in>
2020-06-24 17:09:18 +0530

    add npencoder function

--- generator.py
+++ generator.py
@@ -3,6 +3,18 @@ import numpy as np
 import json
 import datetime
 
+#to handle json serialization
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NpEncoder, self).default(obj)
+
 def find_mode(narration):
     if "UPI" in narration:
         return "UPI"
